40288ec0543f08a6ff4a49b7a410b27193256f19
Miles Spielberg <miles@box.com>
2014-12-22 10:56:17 -0800

    Initial Commit

---
+++ tipcalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipcalculator
+//
+//  Created by Miles Spielberg on 12/22/14.
+//  Copyright (c) 2014 OrionNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tipcalculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tipcalculator
+//
+//  Created by Miles Spielberg on 12/22/14.
+//  Copyright (c) 2014 OrionNet. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
